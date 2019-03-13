9c7098460d5873b5260f1c8efe8ff0f16eea9a5b
dev-nes-ilya <ilya161nesmiyanov@gmail.com>
2019-03-13 17:12:21 +0300

    corrected code
    try again!

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    tasks: 'первая задача'
-  };
-  
+	state = {
+		tasks: 'первая задача',
+	};
 
-  onChangeInput = (evt) => {
-    const newTask = evt.target.value;
-    this.setState({
-    tasks: newTask
-   })
-  };
+	onChangeInput = evt => {
+		const newTask = evt.target.value;
+		this.setState({
+			tasks: newTask,
+		});
+	};
 
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a> */}
-          <tasks>
-            cheklist: {this.state.tasks}
-          </tasks>
-          <input type="text" onChange={this.onChangeInput}></input>
-          {/* <button onClick={this.onClickButton}>add</button> */}
-        </header>
-        
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<div>cheklist: {this.state.tasks}</div>
+					<input type="text" onChange={this.onChangeInput} />
+				</header>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default App;yarn
