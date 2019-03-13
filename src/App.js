f4396d318fdbfa85976b3cd88807f4c6181bacbe
dev-nes-ilya <ilya161nesmiyanov@gmail.com>
2019-03-13 20:27:41 +0300

    changed format
    no more comment))

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-	state = {
-		tasks: 'первая задача',
-	};
+  state = {
+    tasks: "первая задача"
+  };
 
-	onChangeInput = evt => {
-		const newTask = evt.target.value;
-		this.setState({
-			tasks: newTask,
-		});
-	};
+  onChangeInput = evt => {
+    const newTask = evt.target.value;
+    this.setState({
+      tasks: newTask
+    });
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<div>cheklist: {this.state.tasks}</div>
-					<input type="text" onChange={this.onChangeInput} />
-				</header>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>cheklist: {this.state.tasks}</div>
+          <input type="text" onChange={this.onChangeInput} />
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;yarn
+export default App;
